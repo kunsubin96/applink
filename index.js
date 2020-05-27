b111927aa97e5aa6ab0c5caaa1e85597a18c68e5
Ky <lqky@lacviet.com.vn>
2020-05-27 16:48:42 +0700

    d

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs=require('fs');
 const express = require('express')
 const app = express()
-const port = 3000
 var assetlinks = fs.readFileSync(__dirname + '/assetlinks.json');
 
 
@@ -9,4 +8,4 @@ var assetlinks = fs.readFileSync(__dirname + '/assetlinks.json');
 
 app.get('/.well-known/assetlinks.json', (req, res) => res.send(assetlinks))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || 8000, () => console.log(`Example app listening`))
